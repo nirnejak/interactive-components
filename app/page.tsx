9e4c1e382ee4414a1250ff5f7a57700990d062d3
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-01 16:15:04 +0530

    Dynamic Island Component integrated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
+import DynamicIsland from "@/components/DynamicIsland"
 import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
@@ -13,9 +14,7 @@ export const metadata: Metadata = generateMetadata({
 const Home: React.FC = () => {
   return (
     <main className="grid h-screen place-content-center">
-      <h1 className="text-center text-xl font-medium text-zinc-300">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <DynamicIsland />
     </main>
   )
 }
