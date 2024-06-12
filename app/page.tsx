5022ecdd02c2609cd3e76257ea8cdfc6750ac762
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-12 23:38:36 +0530

    Component links from list

--- app/page.tsx
+++ app/page.tsx
@@ -14,46 +14,29 @@ export const metadata: Metadata = generateMetadata({
     "A Gallery of interactive components made with React, TailwindCSS, Framer Motion and TypeScript",
 })
 
+const components = [
+  { title: "Dynamic Island", link: "/dynamic-island/" },
+  { title: "Calculate HEX Alpha", link: "/calculate-hex-alpha/" },
+  { title: "Exit Animation", link: "/exit-animation/" },
+  { title: "Slider Tabs", link: "/slider-tabs/" },
+]
+
 const Home: React.FC = () => {
   return (
     <main className="grid h-screen place-content-center">
       <div className="flex max-w-[580px] flex-col gap-3">
-        <Link
-          href="/dynamic-island/"
-          className="group flex items-center gap-1.5 text-zinc-200"
-        >
-          <span>Dynamic Island</span>
-          <span className="transition-all group-hover:translate-x-1">
-            <ArrowRight size={16} />
-          </span>
-        </Link>
-        <Link
-          href="/calculate-hex-alpha/"
-          className="group flex items-center gap-1.5 text-zinc-200"
-        >
-          <span>Calculate HEX Alpha</span>
-          <span className="transition-all group-hover:translate-x-1">
-            <ArrowRight size={16} />
-          </span>
-        </Link>
-        <Link
-          href="/exit-animation/"
-          className="group flex items-center gap-1.5 text-zinc-200"
-        >
-          <span>Exit Animation</span>
-          <span className="transition-all group-hover:translate-x-1">
-            <ArrowRight size={16} />
-          </span>
-        </Link>
-        <Link
-          href="/slider-tabs/"
-          className="group flex items-center gap-1.5 text-zinc-200"
-        >
-          <span>Slider Tabs</span>
-          <span className="transition-all group-hover:translate-x-1">
-            <ArrowRight size={16} />
-          </span>
-        </Link>
+        {components.map((component, i) => (
+          <Link
+            key={i}
+            href={component.link}
+            className="group flex items-center gap-1.5 text-zinc-200"
+          >
+            <span>{component.title}</span>
+            <span className="transition-all group-hover:translate-x-1">
+              <ArrowRight size={16} />
+            </span>
+          </Link>
+        ))}
       </div>
     </main>
   )
