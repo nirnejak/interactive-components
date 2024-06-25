28c33d1a01186b5209f0c0a0778d309503de5913
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-25 18:47:53 +0530

    Dynamic Button Component added

---
+++ components/DynamicButton.tsx
@@ -0,0 +1,54 @@
+"use client"
+import * as React from "react"
+
+import { ArrowCycle } from "akar-icons"
+import { AnimatePresence, motion } from "framer-motion"
+
+const buttonCopy = {
+  idle: "Send link",
+  loading: <ArrowCycle size={16} className="animate-spin" />,
+  success: "Link sent!",
+}
+
+const DynamicButton: React.FC = () => {
+  const [buttonState, setButtonState] = React.useState("idle")
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 text-white">
+      <div className="grid h-64 w-96 place-content-center rounded-lg bg-black p-4 text-center text-sm transition-all ease-out">
+        <button
+          className="h-10 w-28 overflow-hidden rounded bg-zinc-300 px-3 py-1.5 text-sm text-zinc-900"
+          disabled={buttonState === "loading"}
+          onClick={() => {
+            if (buttonState === "success") return
+
+            setButtonState("loading")
+
+            setTimeout(() => {
+              setButtonState("success")
+            }, 1750)
+
+            setTimeout(() => {
+              setButtonState("idle")
+            }, 3500)
+          }}
+        >
+          <AnimatePresence mode="popLayout" initial={false}>
+            <motion.span
+              transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+              initial={{ opacity: 0, y: -25 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 25 }}
+              key={buttonState}
+              className="flex w-full items-center justify-center"
+            >
+              {buttonCopy[buttonState]}
+            </motion.span>
+          </AnimatePresence>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default DynamicButton
